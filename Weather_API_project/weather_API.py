bfe5c3726a43c7d589c1f4aeda1b65a971363a5b
Ben Holburn-Roberts <benholburnroberts@outlook.com>
2025-11-24 17:02:53 +0000

    fixed environment variables issue

--- Weather_API_project/weather_API.py
+++ Weather_API_project/weather_API.py
@@ -2,12 +2,15 @@ from flask import request
 import os
 from flask import Flask
 from flask import jsonify
+import requests
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 
 
+load_dotenv()
 
-
-API_KEY = os.getenv("API_KEY")
-
+API_KEY=os.getenv("API_KEY")
 
 app = Flask(__name__)
 
@@ -16,16 +19,16 @@ app = Flask(__name__)
 
 @app.route("/weather", methods=["GET"])
 def get_weather():
-    filter_start=request.args.get('start',"")
     filter_end=request.args.get('end',"")
+    filter_start=request.args.get('start',"")
     location=request.args.get('location')
+
 
 
     URL=f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{filter_start}/{filter_end}?key={API_KEY}'
 
-    response=request.get(URL)
-
-    return jsonify(response.json)
+    response=requests.get(URL)
+    return jsonify(response.json())
 
 
 
