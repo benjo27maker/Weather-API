f2ab24590f62c7fdcec678b4dc11063494561c0d
Ben Holburn-Roberts <benholburnroberts@outlook.com>
2025-11-30 21:23:07 +0000

    tidied things up and commented my code

--- Weather_API_project/weather_API.py
+++ Weather_API_project/weather_API.py
@@ -10,10 +10,11 @@ import os
 import redis
 import logging
 import json
+
+# loading environment variables
 load_dotenv()
 
-load_dotenv()
-
+# setting up redis cache 
 redis_host='redis-19287.c338.eu-west-2-1.ec2.cloud.redislabs.com'
 redis_port='19287'
 redis_password=os.getenv('redis_password')
@@ -21,6 +22,7 @@ redis_username=os.getenv('redis_username')
 redis_expiry=3600*3
 
 
+# passing redis its information
 r=redis.Redis(
     host=redis_host,
     port=redis_port,
@@ -30,8 +32,10 @@ r=redis.Redis(
 
 
 
-
+# creating server to host API
 app = Flask(__name__)
+
+# setting up rate limiter for API requests
 limiter = Limiter(
     get_remote_address,
     app=app,
@@ -39,39 +43,58 @@ limiter = Limiter(
     storage_uri=f"redis://:{redis_password}@{redis_host}:{redis_port}/0"
 )
 
-
+# creating location route to return weather data
 @app.get("/location")
 def weather_route():
-    city = request.args.get("city", "Edinburgh")  # default if no city
-    return get_weather(city)
+    city = request.args.get("city", "")  
+    filter_start = request.args.get("start", None) 
+    filter_end = request.args.get("end", None) 
+    if filter_start:
+        filter_start = "/" + filter_start
+    else:
+        filter_start = ""
 
-def get_weather(location):
-    filter_end='2025-01-29'
-    filter_start='2025-01-28'
-    location='Edinburgh'
+    if filter_end:
+        filter_end = "/" + filter_end
+    else:
+        filter_end = ""
+    return get_weather(city,filter_start,filter_end)
+
+
+def get_weather(location,filter_start,filter_end):
+    '''this function requests weather data from a 3rd party API
+    
+    args: location (str), filter_start (str), filter_end (str)
+
+    returns: weather data (json) or error message
+    
+    '''
+
+    # creates redis key from http queries
     redis_key=location+filter_start+filter_end
 
-    URL=f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{filter_start}/{filter_end}?key={os.getenv("API_KEY")}'
+    # creates a URL from the location, start time and end time
+    URL=f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline{location}{filter_start}/{filter_end}?key={os.getenv("API_KEY")}'
 
-
+    # trying to fetch data from cache
     try:
         cached_data=r.get(redis_key)
         if cached_data:
             data = json.loads(cached_data.decode('utf-8'))
-            print(json.dumps(data, indent=2))
-            return
-        
+            return json.dumps(data, indent=2)
+    
     except redis.RedisError as redis_error:
         logging.error(f'redis error : {redis_error}')
 
 
 
     
-
+    # fetches data from 3rd party API otherwise
     response=requests.get(URL)
 
     
-
+    # if request successful return data
+    # stores new data in cache
     if response.status_code==200:
         try:
             r.setex(redis_key,redis_expiry,json.dumps(response.json()))
@@ -80,17 +103,18 @@ def get_weather(location):
             logging.error(f'failed to cache response : {redis_error}')
 
         data = response.json()
-        print(json.dumps(data, indent=2),200)
-        return
+        return json.dumps(data, indent=2),200
 
+    # if request is unsuccessful return error message
+    # 3rd party failure
     elif 500<=response.status_code<600:
         logging.error(f'server error occured')
         return f"request failed with response status {response.status_code}",500
-    
+    # error in query format in request
     elif 400<=response.status_code<500:
         logging.error(f"client error occured")
         return f"client error occured request failed with response status {response.status_code}",400
-    
+    # unknown error
     else:
         logging.error(f"unknown error occured")
         return f"an unknown error occured request failed with response status {response.status_code}"
@@ -104,7 +128,7 @@ def get_weather(location):
 
 
 
-
+# creates the server and makes request when the program is run
 if __name__=="__main__":
     app.run(debug=True)
 
